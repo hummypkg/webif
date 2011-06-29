43db44d742c8dd983edf69a56e7d8d26c836798b
hummypkg <af123@hpkg.tv>
2011-06-29 22:53:15 +0000

    improve EPG search using drop-down options. Implement package management
    git-svn-id: file:///root/webif/svn/humax/pkg/src/webif/trunk@210 2a923420-c742-0410-a762-8d5b09965624

--- var/mongoose/html/js/packages.js
+++ var/mongoose/html/js/packages.js
@@ -1,9 +1,4 @@
 
-function reset_results()
-{
-	$('#dresults').empty().val('Running package command,  please wait...');
-}
-
 $(document).ready(function() {
 	$('button').button();
 
@@ -15,9 +10,8 @@ $(document).ready(function() {
 		draggable: true, resizable: true,
 		buttons: { "Close":
 		    function() {$(this).dialog('close');}},
-		close: function(e,u) { reset_results(); }
+		close: function(e,u) { window.location.reload(true); }
 	});
-	reset_results();
 
 	var opkg = '/cgi-bin/opkg.jim?cmd=';
 
@@ -26,21 +20,13 @@ $(document).ready(function() {
 		$('#results').show('slow');
 	});
 
-	$('button.remove').click(function() {
-		$('#dresults').load(opkg +
-		    encodeURIComponent('remove ' + $(this).attr('id')));
-		$dialog.dialog('open');
-	});
-
-	$('button.install').click(function() {
-		$('#dresults').load(opkg +
-		    encodeURIComponent('install ' + $(this).attr('id')));
-		$dialog.dialog('open');
-	});
-
-	$('button.upgrade').click(function() {
-		$('#dresults').load(opkg +
-		    encodeURIComponent('upgrade ' + $(this).attr('id')));
+	$('button.remove, button.install, button.upgrade').click(function() {
+		$('#dspinner').show();
+		$('#dresults').load(
+		    opkg + encodeURIComponent(
+		        $(this).attr('action') + ' ' + $(this).attr('id')
+		    ),
+		    function() { $('#dspinner').hide('slow') });
 		$dialog.dialog('open');
 	});
 });
