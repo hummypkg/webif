64b73ee14b9739fd9eecf8253d55d7bd943b8df2
hummypkg <af123@hpkg.tv>
2011-07-14 22:22:54 +0000

    create chunked module and add iajax. Update pkg to handled chunked responses
    git-svn-id: file:///root/webif/svn/humax/pkg/src/webif/trunk@273 2a923420-c742-0410-a762-8d5b09965624

---
+++ var/mongoose/html/js/iajax.js
@@ -0,0 +1,50 @@
+
+jQuery.ajaxPrefilter(function(options, _, jqXHR) {
+
+    if (jQuery.isFunction(options.progress)) {
+
+        var xhrFactory = options.xhr,
+            interval;
+
+        options.xhr = function() {
+            
+            var xhr = xhrFactory.apply(this, arguments),
+                partial = "",
+                prevcount = 1;
+
+            interval = setInterval(function() {
+
+                var responseText,
+                    jQueryPartial;
+
+                try {
+
+                    responseText = xhr.responseText;
+                    
+                    if (responseText && (responseText.length > partial.length))
+		    {
+
+                        partial = responseText;
+                        jQueryPartial = $(partial).filter("*")
+
+                        if (jQueryPartial.length > prevcount) {
+                            prevcount = jQueryPartial.length;
+                            options.progress(jQueryPartial.filter("*:not(:last)"));
+                        }
+                    }
+                } catch(e) {
+                    alert(e);
+                }
+            }, options.progressInterval);
+
+            return xhr;
+        };
+        function stop()
+	{
+            if (interval)
+                clearInterval(interval);
+        }
+        jqXHR.then(stop, stop);
+    }
+});
+
