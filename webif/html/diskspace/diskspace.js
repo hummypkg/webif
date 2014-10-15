67ffdc81053e23914533d06294b2b6d47948d510
hummypkg <af123@hpkg.tv>
2014-10-15 19:31:43 +0000

    new pie chart
    git-svn-id: file:///root/webif/svn/humax/pkg/src/webif/trunk@2055 2a923420-c742-0410-a762-8d5b09965624

---
+++ webif/html/diskspace/diskspace.js
@@ -0,0 +1,59 @@
+//$(function () {
+
+$('#tbdiskpie')
+    .hover(function(e) { e.stopPropagation(); $(this).stop(true, true); })
+    .highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+	    backgroundColor: 'transparent',
+            plotShadow: false,
+	    spacing: [0, 0, 0, 0],
+	    className: 'tbdiskpie',
+//	    height: 150, width: 150,
+	    events: {
+		click: function(e) {
+			window.location = '/diag/dspace/index.jim';
+		}
+	    }
+        },
+	colors:
+	  Highcharts.map(['#7cb5ec', '#e4d354', '#cccccc'], function(color) {
+		return {
+		    radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+		    stops: [
+			[0, color],
+			[1, Highcharts.Color(color).brighten(-0.3).get('rgb')]
+		    ]
+		};
+	}),
+	title: { text: '' },
+	credits: false,
+        plotOptions: {
+            pie: {
+		animation: { duration: 300 },
+                allowPointSelect: false,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+		shadow: false,
+		borderWidth: 0,
+		states: { hover: false }
+            }
+        },
+	tooltip: {
+		enabled: true,
+		formatter: function() {
+			return this.point.name + ': ' + this.point.pretty;
+		}
+	},
+        series: [{
+            type: 'pie',
+            name: 'Disk Space',
+            data: diskspace_data
+        }]
+    });
+
+//});
+
