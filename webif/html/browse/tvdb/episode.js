5f04fbdf019a5a033783c3423abd566548167f29
hummypkg <af123@hpkg.tv>
2015-11-18 23:46:47 +0000

    more tvdb work
    git-svn-id: file:///root/webif/svn/pkg/webif/trunk@2542 2a923420-c742-0410-a762-8d5b09965624

---
+++ webif/html/browse/tvdb/episode.js
@@ -0,0 +1,42 @@
+
+$(function() {
+
+var synopsis = $('.synopsis').text();
+var words = synopsis.split(" ");
+for (var i = 0;i < words.length - 1; i++)
+{
+	var word = words[i];
+	if (word.length > 4)
+		$('table.results').highlight(words[i]);
+}
+
+$('button.select').button().on('click', function() {
+	$tr = $(this).closest('tr');
+	$.get('select.jim', {
+	    file: file,
+	    episode: $tr.attr('sid'),
+	    s: $tr.attr('s'),
+	    e: $tr.attr('e')
+	}, function() {
+		window.location.href = '/browse/?dir=' + 
+		    encodeURIComponent(dir);
+	});
+});
+
+$('#tvdbepsearch').button({icons:{primary:"ui-icon-search"}})
+    .on('click', function() {
+	window.location.href = 'episode.jim?file=' +
+	    encodeURIComponent(file) + '&search=' +
+	    encodeURIComponent($('#searchterm').val());
+});
+
+$('table.results')
+    .tablesorter({
+	theme: 'webif',
+	widthFixed: false,
+	sortList: [[1,1]],
+	widgets: ['zebra', 'stickyHeaders']
+    });
+
+});
+
